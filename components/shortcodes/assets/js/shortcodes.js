01b529ffde5338bc6fcee5e562be457728830ba8
Sazzad Hussain <wedcoder09@gmail.com>
2014-05-25 13:46:15 +0600

    Adding shortcodes

---
+++ components/shortcodes/assets/js/shortcodes.js
@@ -0,0 +1,72 @@
+/* jQuery CounTo */
+(function(a){a.fn.countTo=function(g){g=g||{};return a(this).each(function(){function e(a){a=b.formatter.call(h,a,b);f.html(a)}var b=a.extend({},a.fn.countTo.defaults,{from:a(this).data("from"),to:a(this).data("to"),speed:a(this).data("speed"),refreshInterval:a(this).data("refresh-interval"),decimals:a(this).data("decimals")},g),j=Math.ceil(b.speed/b.refreshInterval),l=(b.to-b.from)/j,h=this,f=a(this),k=0,c=b.from,d=f.data("countTo")||{};f.data("countTo",d);d.interval&&clearInterval(d.interval);d.interval=
+setInterval(function(){c+=l;k++;e(c);"function"==typeof b.onUpdate&&b.onUpdate.call(h,c);k>=j&&(f.removeData("countTo"),clearInterval(d.interval),c=b.to,"function"==typeof b.onComplete&&b.onComplete.call(h,c))},b.refreshInterval);e(c)})};a.fn.countTo.defaults={from:0,to:0,speed:1E3,refreshInterval:100,decimals:0,formatter:function(a,e){return a.toFixed(e.decimals)},onUpdate:null,onComplete:null}})(jQuery);
+
+jQuery(document).ready(function($){
+	$(".tallykit-shortcode-accordion").accordion({heightStyle: "content"});
+	$(".tallykit-shortcode-alert-close").click(function() { $( this ).parent( ".tallykit-shortcode-alert-block" ).hide( "slow"); });
+	$(".tallykit-shortcode-tabs").tabs({
+		fx: { opacity: 'toggle' },
+		select: function(event, ui) {
+			$(this).css('height', jQuery(this).height());
+			$(this).css('overflow', 'hidden');
+		},
+		show: function(event, ui) {
+			$(this).css('height', 'auto');
+			$(this).css('overflow', 'visible');
+		}	
+	});
+	
+	$("h3.tallykit-shortcode-toggle-trigger").click(function(){$(this).toggleClass("active").next().slideToggle("fast");return false;});
+	
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		animation_speed: 'fast', /* fast/slow/normal */
+		slideshow: 5000, /* false OR interval time in ms */
+		autoplay_slideshow: false, /* true/false */
+		opacity: 0.80, /* Value between 0 and 1 */
+		show_title: true, /* true/false */
+		allow_resize: true, /* Resize the photos bigger than viewport. true/false */
+		default_width: 500,
+		default_height: 344,
+		counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
+		theme: 'pp_default', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
+		horizontal_padding: 20, /* The padding on each side of the picture */
+		hideflash: false, /* Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto */
+		wmode: 'opaque', /* Set the flash wmode attribute */
+		autoplay: true, /* Automatically start videos: True/False */
+		modal: false, /* If set to true, only the close button will close the window */
+		deeplinking: false, /* Allow prettyPhoto to update the url to enable deeplinking. */
+		overlay_gallery: true, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
+		keyboard_shortcuts: true, /* Set to false if you open forms inside prettyPhoto */
+		changepicturecallback: function(){}, /* Called everytime an item is shown/changed */
+		callback: function(){}, /* Called when prettyPhoto is closed */
+		ie6_fallback: true,
+		custom_markup: '',
+		social_tools: false /* html or false to disable */
+	});
+	
+	if(jQuery().waypoint) {
+		jQuery('.tallykit-shortcode-counterBox-wrapper').waypoint(function() {
+			jQuery(this).find('.display-percentage').each(function() {
+				var percentage = jQuery(this).data('percentage');
+				jQuery(this).countTo({from: 0, to: percentage, speed: 300});
+			});
+		}, {
+			triggerOnce: true,
+			offset: '100%'
+		});
+	}
+	
+	if(jQuery().waypoint) {
+		jQuery('.tallykit-shortcode-progressBar').waypoint(function() {
+			var percentage = jQuery(this).find('.tallykit-shortcode-progressBar-content').data('percentage');
+			jQuery(this).find('.tallykit-shortcode-progressBar-content').css('width', '0%');
+			jQuery(this).find('.tallykit-shortcode-progressBar-content').animate({
+				width: percentage+'%'
+			}, 'slow');
+		}, {
+			triggerOnce: true,
+			offset: '100%'
+		});
+	}
+});
--- components/shortcodes/shortcodes.php
+++ components/shortcodes/shortcodes.php
@@ -7,6 +7,7 @@
  *
  * @package TallyKit
  * @subpackage Shortcodes
+ * @class tallykit-shortcode
  */
  
 // Exit if accessed directly
@@ -58,6 +59,9 @@ function tallykit_shortcodes_script_loader(){
 	wp_enqueue_script('jquery-easing');
 	wp_enqueue_script('jquery-fitvids');
 	
+	wp_enqueue_script('jquery-gauge');
+	wp_enqueue_script('jquery-waypoints');
+	
 	//prettyPhoto
 	wp_enqueue_style( 'jquery-prettyPhoto');
 	wp_enqueue_script('jquery-prettyPhoto');
@@ -94,88 +98,351 @@ function tallykit_shortcodes_sc_(){
 }
 
 
+
 /*---------|- accordion -|-------------------------------------*/
 add_shortcode('tk_accordion', 'tallykit_shortcodes_sc_accordion');
-function tallykit_shortcodes_sc_accordion(){
-	
+function tallykit_shortcodes_sc_accordion( $atts, $content = null  ) {
+	extract( shortcode_atts( array(
+		'class'	=> ''
+	), $atts ) );
+	return '<div class="tallykit-shortcode-accordion '. $class .'">' . do_shortcode($content) . '<span class="acoc-clear"></span></div>';
 }
+
+add_shortcode('tk_accordion_item', 'tallykit_shortcodes_sc_accordion_item');
+function tallykit_shortcodes_sc_accordion_item( $atts, $content = null  ) {
+	extract( shortcode_atts( array(
+		'title'	=> 'Title',
+		'class'	=> '',
+	), $atts ) );
+	$output = '';
+	
+	$output .= '<h3 class="tallykit-shortcode-accordion-trigger '. $class .'">';
+		$output .= '<a href="#">'. $title .'</a>';
+	$output .= '</h3>';
+	$output .= '<div>';
+		$output .= do_shortcode($content);
+		$output .= '<div class="acoc-clear"></div>';
+	$output .= '</div>';
+	
+	return $output; 
+}
+
 
 
 /*---------|- alert -|-------------------------------------*/
 add_shortcode('tk_alert', 'tallykit_shortcodes_sc_alert');
-function tallykit_shortcodes_sc_alert(){
+function tallykit_shortcodes_sc_alert( $atts, $content = null  ) {
+	extract( shortcode_atts( array(
+		'title' => '',
+		'type' => 'info', //danger, success, error, info, 
+		'close' => 'yes',
+	), $atts ) );
 	
+	$output = '';
+	
+	$output .= '<div class="tallykit-shortcode-alert tallykit-shortcode-alert-block tallykit-shortcode-alert-'.$type.'">';
+		if( $close == 'yes'){ $output .= '<div class="tallykit-shortcode-alert-close">×</div>'; }
+		if( $title != ''){ $output .= '<h4 class="tallykit-shortcode-alert-heading">'.$title.'</h4>'; }
+		$output .= do_shortcode($content);
+	$output .= '</div>';
+	
+	return $output;
 }
+
 
 
 /*---------|- button -|-------------------------------------*/
 add_shortcode('tk_button', 'tallykit_shortcodes_sc_button');
-function tallykit_shortcodes_sc_button(){
+function tallykit_shortcodes_sc_button( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'color'			=> '', //red, 
+		'size'			=> '2x', //1x, 2x, 3x, 4x, 5x
+		'link'			=> '#',
+		'title'			=> '',
+		'text'			=> 'Button',
+		'target'		=> '',
+		'rel'			=> '',
+		'border_radius'	=> '',
+		'class'			=> '',
+		'icon_left'		=> '',
+		'icon_right'	=> '',
+		'id'	        => '',
+		'bg_color'      => '',
+		'text_color'    => '',
+		'full_width'    => '', //yes, no
+	), $atts ) );
 	
+	$output = '';
+		
+	//icon
+	$icon_left = ($icon_left) ? '<i class="fa '. $icon_left .'"></i>' : NULL;
+	$icon_right = ($icon_right) ? '<i class="fa '. $icon_right .'"></i>' : NULL;
+	
+	//attribute
+	$rel = ($rel) ? 'rel="'.$rel.'" ':NULL;
+	$target = ($target) ? 'target="'.$target.'" ':NULL;
+	$id = ($id) ? 'id="'.$id.'" ':NULL;
+	$title = ($title) ? 'title="'.$title.'" ':NULL;
+		
+	//class
+	$full_width = ($full_width == 'yes') ? 'tallykit-shortcode-button-full-width':NULL;
+	$icon_left_class = ($icon_left) ? 'icon-left' : NULL;
+	$icon_right_class = ($icon_right) ? 'icon-right' : NULL;
+	$button_class = 'class="tallykit-shortcode-button '.$full_width.' '.$class.' size-'.$size.' color-'.$color.' '.$icon_left_class.' '.$icon_right_class.'" ';
+		
+	//style
+	$radius = ( $border_radius) ? 'border-radius:'. $border_radius .';' : NULL;
+	$bg_color = ( $bg_color) ? 'background-color:'. $bg_color .';' : NULL;
+	$text_color = ( $text_color) ? 'color:'. $text_color .';' : NULL;
+	$button_style = 'style="'.$radius.$bg_color.$text_color.'" ';
+		
+	$output .= '<a href="'.$link.'" '.$button_class.$rel.$target.$id.$title.$button_style.'>'.$icon_left.$text.$icon_right.'</a>';
+	
+	return $output;
 }
+
 
 
 /*---------|- checklist -|-------------------------------------*/
 add_shortcode('tk_checklist', 'tallykit_shortcodes_sc_checklist');
-function tallykit_shortcodes_sc_checklist(){
+function tallykit_shortcodes_sc_checklist( $atts, $content = null ) {
+	extract(shortcode_atts(array(
+		'icon' => 'arrow', //check, star, arrow, asterik, cross, plus
+		'iconcolor' => 'initial',
+		'iconbg' => 'rgba(0,0,0,0)',
+		'iconsize' => '9px',
+		'circle' => 'yes'
+	), $atts));
 	
+	$rand = rand();
+	$output = '';
+	$output .= "<style type='text/css'>
+	#tallykit-shortcode-checklist-".$rand." ul li:before{color:{$iconcolor} !important;}
+	#tallykit-shortcode-checklist-".$rand." ul li:before{background-color:{$iconbg} !important;}
+	#tallykit-shortcode-checklist-".$rand." ul li:before{font-size:{$iconsize} !important;}
+	</style>";
+	
+	$output .= '<div class="tallykit-shortcode-checklist tallykit-shortcode-checklist-circle-'.$circle.' tallykit-shortcode-checklist-icon-'.$icon.'" id="tallykit-shortcode-checklist-'.$rand.'">';
+		$output .= do_shortcode($content);
+	$output .= '</div>';
+	
+	return $output;	
 }
+
 
 
 /*---------|- column -|-------------------------------------*/
 add_shortcode('tk_column', 'tallykit_shortcodes_sc_column');
-function tallykit_shortcodes_sc_column(){
-	
+function tallykit_shortcodes_sc_column( $atts, $content = null ){
+	extract( shortcode_atts( array(
+		'size'		=> 'one-third', //one-half, one-third, two-third, 
+		'position'	=>'first',
+		'class'		=> '',
+		'bg_color' => '',
+		'text_align' => '',
+		'text_color' => '',
+		'heading_color' => '',
+		'link_color' => '',
+		'padding' => '',
+	  ), $atts ) );
+		  
+	  $output = '';
+	  $uid = 'tallykit-shortcode-column'.rand();
+	  $bg_color = ($bg_color) ? 'background-color:'.$bg_color.';' : '';
+	  $padding = ($padding) ? 'padding:'.$padding.';' : '';
+		  
+	  $output .= '<style type="text/css">
+				#'.$uid.'{ color:'.$text_color.'; }
+				#'.$uid.' a{ color:'.$link_color.'; }
+				#'.$uid.' h1, #'.$uid.' h2, #'.$uid.' h3, #'.$uid.' h4, #'.$uid.' h5, #'.$uid.' h6{ color:'.$heading_color.'; }
+			</style>';
+		  
+	$output .= '<div id="'.$uid.'" class="tallykit-shortcode-column tallykit-shortcode-' . $size . ' tallykit-shortcode-column-'.$position.' '. $class .'" style="'.$bg_color.'">';
+		$output .= '<div class="tallykit-shortcode-column-inner" style="'.$padding.'">' . do_shortcode($content) . '</div>';
+	$output .= '</div>';
+	   
+	return $output;
 }
+
 
 
 /*---------|- divider -|-------------------------------------*/
 add_shortcode('tk_divider', 'tallykit_shortcodes_sc_divider');
-function tallykit_shortcodes_sc_divider(){
+function tallykit_shortcodes_sc_divider($atts, $content = null) {
+	extract(shortcode_atts(array(
+		'style' => 'none', //single, double, dotted, dashed, shadow
+		'margin_top' => '20px',
+		'margin_bottom' => '2px',
+	), $atts));
 	
+	$output = '';
+	$rand = rand();
+	
+	$output .= '<style type="text/css">#tallykit-shortcode-divider-'.$rand.'{  margin-bottom:'.$margin_bottom.'; margin-top:'.$margin_top.'; }</style>';
+	$output .= '<div class="acoc-clear"></div>';
+	$output .= '<div class="tallykit-shortcode-divider tallykit-shortcode-divider-style-'.$style.'" id="tallykit-shortcode-divider-'.$rand.'"></div>';
+	$output .= '<div class="acoc-clear"></div>';
+	
+	return $output;
 }
+
 
 
 /*---------|- dropcap -|-------------------------------------*/
 add_shortcode('tk_dropcap', 'tallykit_shortcodes_sc_dropcap');
-function tallykit_shortcodes_sc_dropcap(){
-	
+function tallykit_shortcodes_sc_dropcap( $atts, $content = null ) {  
+	extract(shortcode_atts(array(
+		'style' => 'none', //circle, box, round
+	), $atts));
+	$output = '';
+	$rand = rand();
+	$output .= '<span class="tallykit-shortcode-dropcap tallykit-shortcode-dropcap-style-'.$style.'" id="tallykit-shortcode-dropcap-'.$rand.'">'.do_shortcode($content).'</span>';
+	return $output;
 }
+
 
 
 /*---------|- icon -|-------------------------------------*/
 add_shortcode('tk_icon', 'tallykit_shortcodes_sc_icon');
-function tallykit_shortcodes_sc_icon(){
+function tallykit_shortcodes_sc_icon( $atts, $content = null ) {  
+	extract(shortcode_atts(array(
+		'icon' => 'fa-arrows',
+		'shape' => '', //circle, round,
+		'style' => 'none', //background, border
+		'color' => '#666',
+		'size' => '2x', // 1x, 2x, 3x, 4x, 5x, 6x
+		'effect' => '', //rotate, fade
+		'align' => '', //none, left, right, center
+		'link' => '',
+		'link_target' => '_self', //_blank, _self
+	), $atts));
 	
+	$output = '';
+	$rand = rand();
+	
+	$output .= '<style type="text/css">
+		#shortallykit-shortcodetwell-icon-'.$rand.'{ color:'.$color.'; }
+		#tallykit-shortcode-icon-'.$rand.'.tallykit-shortcode-icon-style-background{ background-color:'.$color.'; color:#ffffff !important; }
+		#tallykit-shortcode-icon-'.$rand.'.tallykit-shortcode-icon-style-border{ border-color:'.$color.'; }
+	</style>';
+	
+	if(!empty($link)){ $output .= '<a href="'.$link.'" target="'.$link_target.'">'; }
+	$output .= '<i class="tallykit-shortcode-icon  tallykit-shortcode-icon-shape-'.$shape.'  tallykit-shortcode-icon-size-'.$size.' tallykit-shortcode-icon-effect-'.$effect.' tallykit-shortcode-icon-align-'.$align.' tallykit-shortcode-icon-style-'.$style.' fa '.$icon.'" id="tallykit-shortcode-icon-'.$rand.'"></i>';
+	if(!empty($link)){ $output .= '</a>'; }
+	
+	return $output;	
 }
+
 
 
 /*---------|- highlight -|-------------------------------------*/
 add_shortcode('tk_highlight', 'tallykit_shortcodes_sc_highlight');
-function tallykit_shortcodes_sc_highlight(){
-	
+function tallykit_shortcodes_sc_highlight($atts, $content = null) {
+	$atts = shortcode_atts(array(
+		'color' => 'yellow',
+	), $atts);
+			
+	if($atts['color'] == 'black') {
+		return '<span class="tallykit-shortcode-highlight2" style="background-color:'.$atts['color'].'; color:#fff;">' .do_shortcode($content). '</span>';
+	} else {
+		return '<span class="tallykit-shortcode-highlight1" style="background-color:'.$atts['color'].';">' .do_shortcode($content). '</span>';
+	}
 }
 
-
-/*---------|- image -|-------------------------------------*/
-add_shortcode('tk_image', 'tallykit_shortcodes_sc_image');
-function tallykit_shortcodes_sc_image(){
-	
-}
 
 
 /*---------|- lightbox -|-------------------------------------*/
 add_shortcode('tk_lightbox', 'tallykit_shortcodes_sc_lightbox');
-function tallykit_shortcodes_sc_lightbox(){
+function tallykit_shortcodes_sc_lightbox($atts, $content = null) {
+	extract( shortcode_atts( array(
+		'class' => '',
+		'src' => '',
+		'title' => '',
+	), $atts ) );
 	
+	$uid = 'tallykit-shortcode-lightbox'.rand();	
+	$output = '';
+	$output .= '<a href="'.$src.'" rel="prettyPhoto" title="'.$title.'" class="'.$class.'">';
+		$output .= do_shortcode($content);
+	$output .= '</a>'; 
+
+	return $output;
 }
+
 
 
 /*---------|- map -|-------------------------------------*/
 add_shortcode('tk_map', 'tallykit_shortcodes_sc_map');
-function tallykit_shortcodes_sc_map(){
-	
+function tallykit_shortcodes_sc_map($atts, $content = null) {
+
+	extract(shortcode_atts(array(
+		'address' => '',
+		'type' => 'satellite', // ROADMAP, SATELLITE, HYBRID, TERRAIN
+		'width' => '100%',
+		'height' => '300px',
+		'zoom' => '15',
+		'scrollwheel' => 'true',
+		'scale' => 'true',
+		'popup' => 'true',
+		'zoom_pancontrol' => 'true',
+		'icon' => NULL,
+	), $atts));
+
+	$uid = 'tallykit-shortcode-map'.rand();
+
+	if($scrollwheel == 'yes') {
+		$scrollwheel = 'true';
+	} elseif($scrollwheel == 'no') {
+		$scrollwheel = 'false';
+	}
+
+	if($scale == 'yes') {
+		$scale = 'true';
+	} elseif($scale == 'no') {
+		$scale = 'false';
+	}
+
+	if($zoom_pancontrol == 'yes') {
+		$zoom_pancontrol = 'true';
+	} elseif($zoom_pancontrol == 'no') {
+		$zoom_pancontrol = 'false';
+	}
+
+	$addresses = explode('|', $address);
+	if($icon){ $icon = "icon: '".$icon."', "; }
+
+	$markers = '';
+	foreach($addresses as $address_string) {
+		$markers .= "{
+			address: '{$address_string}',
+			".$icon."
+			html: {
+				content: '{$address_string}',
+				popup: {$popup}
+			} 
+		},";	
+	}
+
+	$html = "<script type='text/javascript'>
+	jQuery(document).ready(function($) {
+		jQuery('#".$uid."').goMap({
+			address: '{$addresses[0]}',
+			zoom: {$zoom},
+			scrollwheel: {$scrollwheel},
+			scaleControl: {$scale},
+			navigationControl: {$zoom_pancontrol},
+			maptype: '{$type}',
+	        markers: [{$markers}]
+		});
+	});
+	</script>";
+
+	$html .= '<div class="tallykit-shortcode-map" id="'.$uid.'" style="width:'.$width.';height:'.$height.';">';
+	$html .= '</div>';
+
+	return $html;
 }
+
 
 
 /*---------|- blog -|-------------------------------------*/
@@ -185,18 +452,127 @@ function tallykit_shortcodes_sc_blog(){
 }
 
 
+
 /*---------|- progress-bar -|-------------------------------------*/
 add_shortcode('tk_progress_bar', 'tallykit_shortcodes_sc_tk_progress_bar');
-function tallykit_shortcodes_sc_tk_progress_bar(){
-	
+function tallykit_shortcodes_sc_tk_progress_bar($atts, $content = null) {
+	extract(shortcode_atts(array(
+		'filled_color' => '#45b900',
+		'unfilled_color' => '#f0f0f0',
+		'value' => '70'
+	), $atts));
+
+	$output = '';
+	$output .= '<div class="tallykit-shortcode-progressBar" style="background-color:'.$unfilled_color.' !important;border-color:'.$unfilled_color.' !important;">';
+		$output .= '<div class="tallykit-shortcode-progressBar-content" data-percentage="'.$value.'" style="width: ' . $value . '%;background-color:'.$filled_color.' !important;border-color:'.$filled_color.' !important;">';
+		$output .= '</div>';
+		$output .= '<span class="tallykit-shortcode-progressBar-title">' . $content .'</span>';
+	$output .= '</div>';
+
+	return $output;
 }
+
+
+
+/*---------|- Counter Box -|-------------------------------------*/
+add_shortcode('tk_counter_box', 'tallykit_shortcodes_sc_tk_counter_box');
+function tallykit_shortcodes_sc_tk_counter_box($atts, $content = null) {
+	extract(shortcode_atts(array(
+		'value' => '70'
+	), $atts));
+
+	$output = '';
+	$output .= '<div class="tallykit-shortcode-counterBox-wrapper">';
+		$output .= '<div class="tallykit-shortcode-counterBox-percentage">';
+			$output .= '<span class="display-percentage" data-percentage="'.$value.'">0</span><span class="percent">%</span>';
+		$output .= '</div>';
+		$output .= '<div class="tallykit-shortcode-counterBox-content">';
+			$output .= do_shortcode($content);
+		$output .= '</div>';
+	$output .= '</div>';
+
+	return $output;
+}
+
+
+
+/*---------|- Counter (circle) -|-------------------------------------*/
+add_shortcode('tk_counter_circle', 'tallykit_shortcodes_sc_tk_counter_circle');
+function tallykit_shortcodes_sc_tk_counter_circle($atts, $content = null) {
+	extract(shortcode_atts(array(
+		'filled_color' => '#45b900',
+		'unfilled_color' => '#f1f1f1',
+		'value' => '70'
+	), $atts));
+
+	$uid = 'tallykit_shortcode_CounterCircle'.rand();
+
+	$output = "<script type='text/javascript'>
+	jQuery(document).ready(function() {
+		var opts = {
+		  lines: 12, // The number of lines to draw
+		  angle: 0.5, // The length of each line
+		  lineWidth: 0.05, // The line thickness
+		  colorStart: '".$filled_color."',   // Colors
+		  colorStop: '".$filled_color."',    // just experiment with them
+		  strokeColor: '".$unfilled_color."',   // to see which ones work best for you
+		  generateGradient: false
+		};
+		var gauge_".$uid." = new Donut(document.getElementById('".$uid."')).setOptions(opts);
+		gauge_".$uid.".maxValue = 100; // set max gauge value
+		gauge_".$uid.".animationSpeed = 128; // set animation speed (32 is default value)
+		gauge_".$uid.".set(".$value."); // set actual value
+	});
+	</script>";
+
+
+	$output .= '<div class="tallykit-shortcode-CounterCircle-wrapper">';
+		$output .= '<canvas width="220" height="220" class="tallykit-shortcode-CounterCircle" id="'.$uid.'">';
+		$output .= '</canvas>';
+		$output .= '<div class="tallykit-shortcode-CounterCircle-content">';
+			$output .= do_shortcode($content);
+		$output .= '</div>';
+	$output .= '</div>';
+
+	return $output;
+}
+
 
 
 /*---------|- tab -|-------------------------------------*/
 add_shortcode('tk_tab', 'tallykit_shortcodes_sc_tab');
-function tallykit_shortcodes_sc_tab(){
-	
+function tallykit_shortcodes_sc_tab( $atts, $content = null ) {
+	$defaults = array();
+	extract( shortcode_atts( $defaults, $atts ) );
+	preg_match_all( '/tab_item title="([^\"]+)"/i', $content, $matches, PREG_OFFSET_CAPTURE );
+	$tab_titles = array();
+	if( isset($matches[1]) ){ $tab_titles = $matches[1]; }
+	$output = '';
+	if( count($tab_titles) ){
+	    $output .= '<div id="tallykit-shortcode-tab-'. rand(1, 100) .'" class="tallykit-shortcode-tabs">';
+		$output .= '<ul class="ui-tabs-nav ">';
+		foreach( $tab_titles as $tab ){
+			$output .= '<li><a href="#tallykit-shortcode-tab-'. sanitize_title( $tab[0] ) .'">' . $tab[0] . '</a></li>';
+		}
+	    $output .= '</ul><div class="acoc-clear"></div>';
+	    $output .= do_shortcode( $content );
+	    $output .= '</div>';
+	} else {
+		$output .= do_shortcode( $content );
+	}
+	return $output;
 }
+
+add_shortcode('tk_tab_item', 'tallykit_shortcodes_sc_tab_item');
+function tallykit_shortcodes_sc_tab_item( $atts, $content = null ) {
+	$defaults = array(
+			'title'	=> 'Tab',
+			'class'	=> ''
+	);
+	extract( shortcode_atts( $defaults, $atts ) );
+	return '<div id="tallykit-shortcode-tab-'. sanitize_title( $title ) .'" class="tab-content '. $class .'">'. do_shortcode( $content ) .'</div>';
+}
+
 
 
 /*---------|- callout -|-------------------------------------*/
@@ -206,11 +582,19 @@ function tallykit_shortcodes_sc_callout(){
 }
 
 
+
 /*---------|- toggle -|-------------------------------------*/
 add_shortcode('tk_toggle', 'tallykit_shortcodes_sc_toggle');
-function tallykit_shortcodes_sc_toggle(){
+function tallykit_shortcodes_sc_toggle( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'title'	=> 'Toggle Title',
+		'class'	=> '',
+	), $atts ) );
 	
+	// Display the Toggle
+	return '<div class="tallykit-shortcode-toggle '. $class .'"><h3 class="tallykit-shortcode-toggle-trigger">'. $title .'</h3><div class="tallykit-shortcode-toggle-container">' . do_shortcode($content) . '</div></div>';
 }
+
 
 
 /*---------|- tooltip -|-------------------------------------*/
@@ -220,6 +604,7 @@ function tallykit_shortcodes_sc_tooltip(){
 }
 
 
+
 /*---------|- video -|-------------------------------------*/
 add_shortcode('tk_video', 'tallykit_shortcodes_sc_video');
 function tallykit_shortcodes_sc_video(){
@@ -227,11 +612,13 @@ function tallykit_shortcodes_sc_video(){
 }
 
 
+
 /*---------|- audio -|-------------------------------------*/
 add_shortcode('tk_audio', 'tallykit_shortcodes_sc_audio');
 function tallykit_shortcodes_sc_audio(){
 	
 }
+
 
 
 /*---------|- heading -|-------------------------------------*/
@@ -253,18 +640,29 @@ function tallykit_shortcodes_sc_heading(){
  *
  * @uses class acoc_tinymce_register  
  */
-   new acoc_tinymce_register;
  
  
  
  
  
  
- /*************************** Widgets *****************************
+ /*************************** Shortcode fix *****************************
  *
- * Register Widgets
+ * Remove unwanted <br> and <p> tags
  *
  * @since TallyKit (1.0)
  *
- * @uses class acoc_widget_register  
+ * @uses filter the_content  
  */
+if( !function_exists('tallykit_shortcodes_fix_shortcodes') ) {
+	function tallykit_shortcodes_fix_shortcodes($content){   
+		$array = array (
+			'<p>['		=> '[', 
+			']</p>'		=> ']', 
+			']<br />'	=> ']'
+		);
+		$content = strtr($content, $array);
+		return $content;
+	}
+	add_filter('the_content', 'tallykit_shortcodes_fix_shortcodes');
+}
