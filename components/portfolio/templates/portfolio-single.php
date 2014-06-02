10c5fa9c194c4bf045e6e1fc2979626609a53df7
Sazzad Hussain <wedcoder09@gmail.com>
2014-06-02 17:45:28 +0600

    fix portfolio single template

--- components/portfolio/templates/portfolio-single.php
+++ components/portfolio/templates/portfolio-single.php
@@ -5,7 +5,12 @@ $portfolio_query = new WP_Query( $query );
 	<?php if( $portfolio_query->have_posts()): ?>
 		<?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
 			
-            <img src="<?php echo acoc_image_size(get_post_meta(get_the_ID(), 'tallykit_portfolio_single_image', true) , $width = '960', $height = '450', $crop = true); ?>" width="" height="" alt=""  />
+            <?php if(get_post_meta(get_the_ID(), 'tallykit_portfolio_video', true) != ''): ?>
+            	<?php echo wp_oembed_get(get_post_meta(get_the_ID(), 'tallykit_portfolio_video', true)); ?>
+            <?php else: ?>
+            	<img src="<?php echo acoc_image_size(get_post_meta(get_the_ID(), 'tallykit_portfolio_single_image', true) , $width = '960', $height = '450', $crop = true); ?>" width="" height="" alt="<?php the_title(); ?>"  />
+            <?php endif; ?>
+            
 			<div class="tk_portfolio_single_content">
             	<h3><?php _e('Project Description', 'tallykit_portfolio'); ?></h3>
                 <?php the_content(); ?>
@@ -13,30 +18,36 @@ $portfolio_query = new WP_Query( $query );
             <div class="tk_portfolio_single_info">
             	<h3><?php _e('Project Details', 'tallykit_portfolio'); ?></h3>
     			<ul>
-                    <li>
-                        <strong><?php _e('Skills Needed', 'tallykit_portfolio'); ?></strong>: 
-                        <div class="project_tk">
-                            <?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_project_url_label', true); ?>
-                        </div>
-                    </li>
-                    <li>
-                        <strong><?php _e('Categories Needed', 'tallykit_portfolio'); ?></strong>: 
-                        <div class="project_tk">
-                            
-                        </div>
-                    </li>
+                	<?php if(get_post_meta(get_the_ID(), 'tallykit_portfolio_project_url', true) != ''): ?>
                     <li>
                         <strong><?php _e('Project URL', 'tallykit_portfolio'); ?></strong>: 
+                        <div class="project_tk">
+                        	<a href="<?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_project_url', true); ?>" target="_blank">
+                            	<?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_project_url_label', true); ?>
+                            </a>
+                        </div>
+                    </li>
+                    <?php endif; ?>
+                    <?php if(get_post_meta(get_the_ID(), 'tallykit_portfolio_client_url', true) != ''): ?>
+                    <li>
+                        <strong><?php _e('Client', 'tallykit_portfolio'); ?></strong>: 
+                        <div class="project_tk">
+                        	<a href="<?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_client_url', true); ?>" target="_blank">
+                            	<?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_client_url_label', true); ?>
+                            </a>
+                        </div>
+                    </li>
+                    <?php endif; ?>
+                    <li>
+                        <strong><?php _e('Categories', 'tallykit_portfolio'); ?></strong>: 
                          <div class="project_tk">
-                            <a href="<?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_project_url', true); ?>" target="_blank"><?php _e('View Project', 'well_textdomain'); ?></a>
+                            <?php echo acoc_post_taxonomys_link(get_the_ID(), 'tallykit_portfolio_category'); ?>
                          </div>
                      </li>
                     <li>
-                        <strong><?php _e('Copyright', 'tallykit_portfolio'); ?></strong>: 
+                        <strong><?php _e('Tags', 'tallykit_portfolio'); ?></strong>: 
                         <div class="project_tk">
-                            <a href="<?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_client_url', true); ?>" target="_blank">
-                                <?php echo get_post_meta(get_the_ID(), 'tallykit_portfolio_client_url_label', true); ?>
-                            </a>
+                            <?php echo acoc_post_taxonomys_link(get_the_ID(), 'tallykit_portfolio_tag'); ?>
                         </div>
                     </li>
         		</ul>
