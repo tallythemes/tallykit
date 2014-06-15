55da3fd1378670451f0655fa185d47a6714e1f71
Sazzad Hussain <wedcoder09@gmail.com>
2014-06-15 14:41:57 +0600

    Fix testimonial templeting and css issue

--- components/testimonial/templates/content/content-grid.php
+++ components/testimonial/templates/content/content-grid.php
@@ -1,16 +1,16 @@
 <div class="tallykit_testimonial_item">
-	<div class="tk_item_content"><?php echo get_the_content(); ?></div>
-	<div class="tk_item_info">
-    	<div class="tk_arrow_wrap"><div class="tk_arrow"></div></div>
+	<div class="tk_testimonial_item_content"><?php echo get_the_content(); ?></div>
+	<div class="tk_testimonial_item_info">
+    	<div class="tk_testimonial_arrow_wrap"><div class="tk_testimonial_arrow"></div></div>
     	<?php $image_url = get_post_meta(get_the_ID(), 'tallykit_testimonial_image', true); ?>
 		<img src="<?php echo acoc_image_size($image_url, $width = '300', $height = '300', $crop = true); ?>" width="" height="" alt=""  />
-		<span class="tk_author_name">
+		<span class="tk_testimonial_author_name">
         	<?php $link = get_post_meta(get_the_ID(), 'tallykit_testimonial_link', true);  ?>
         	<?php if($link): ?><a href="<?php echo get_post_meta(get_the_ID(), 'tallykit_testimonial_link', true); ?>" target="_blank"><?php endif; ?>
 				<?php the_title(); ?>
 			<?php if($link): ?></a><?php endif; ?>
             <br />
-            <span class="tk_author_position">- <?php echo get_post_meta(get_the_ID(), 'tallykit_testimonial_position', true); ?> -</span>
+            <span class="tk_testimonial_author_position">- <?php echo get_post_meta(get_the_ID(), 'tallykit_testimonial_position', true); ?> -</span>
         </span>
 	</div>
 </div>
