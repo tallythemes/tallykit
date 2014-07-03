0cc078c132c531861aaaee8271785ccdf40f8374
Sazzad Hussain <wedcoder09@gmail.com>
2014-07-03 11:09:16 +0600

    Improve Slideshow CSS

--- components/slideshow/slideshow-color.php
+++ components/slideshow/slideshow-color.php
@@ -3,7 +3,7 @@ add_action('wp_head', 'tallykit_slideshow_color_action_hook');
 function tallykit_slideshow_color_action_hook(){
 	?>
     <style type="text/css">	
-		/*border style*/
+		/*Button*/
 		.tallykit_slideshow_item.color_mood_light .tallykit_slideshow_item_inner .tk_slideshow_button{ 
 			border-color:<?php tallykitkit_color('color_headings_light'); ?> !important; color:<?php tallykitkit_color('color_headings_light'); ?> !important; }
 		.tallykit_slideshow_item.color_mood_dark .tallykit_slideshow_item_inner .tk_slideshow_button{ 
@@ -12,6 +12,11 @@ function tallykit_slideshow_color_action_hook(){
 		.tallykit_slideshow_item.color_mood_light .tallykit_slideshow_item_inner .tk_slideshow_button:hover,
 		.tallykit_slideshow_item.color_mood_dark .tallykit_slideshow_item_inner .tk_slideshow_button:hover{
 			border-color:<?php tallykitkit_color('site_accent_color'); ?> !important; background-color:<?php tallykitkit_color('site_accent_color'); ?>; color: #FFF !important; }
+			
+		/*content*/
+		.tallykit_slideshow_item.color_mood_light .tallykit_slideshow_item_inner .tk_slideshow_content{ color:<?php tallykitkit_color('color_headings_light'); ?> !important; }
+		
+		.tallykit_slideshow_item.color_mood_dark .tallykit_slideshow_item_inner .tk_slideshow_content{ color:<?php tallykitkit_color('color_headings_dark'); ?> !important; }
 	</style>
     <?php	
 }
