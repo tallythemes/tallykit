e3a7c458ba70431a23030a44ef329e94b57e24df
Sazzad Hussain <wedcoder09@gmail.com>
2014-07-10 17:50:24 +0600

    fix portfolio image size

--- components/portfolio/templates/content/content-grid.php
+++ components/portfolio/templates/content/content-grid.php
@@ -4,7 +4,7 @@
         	<?php
 			$image_url = get_post_meta(get_the_ID(), 'tallykit_portfolio_archive_image', true);
 			?>
-			<img src="<?php echo acoc_image_size($image_url, $width = '600', $height = '450', $crop = true); ?>" width="" height="" alt=""  />
+			<img src="<?php echo acoc_image_size($image_url, $width = '600', $height = '400', $crop = true); ?>" alt="<?php the_title(); ?>"  />
         </div>
 		<div class="tallykit_portfolio_item_details">
 			<h3 class="tallykit_portfolio_item_heading"><?php the_title(); ?></h3>
