224dee7b13d4551abca0111ab24e29c2c25253cc
Sazzad Hussain <wedcoder09@gmail.com>
2014-09-20 12:29:33 +0600

    Fix a Bug on Gallery template

--- components/gallery/templates/content/single-image.php
+++ components/gallery/templates/content/single-image.php
@@ -1,11 +1,21 @@
 <div class="tk_single_gallery_image">
 	<?php
-	$image_data = get_post( $attachment ); // Get post by ID
-	$link = $image_data->guid;
-	$link_class = 'magnificPopup-child';
-	$link_icon_class = 'fa-search-plus"';
-	$image_url = $image_data->guid;
-	$image_caption = $image_data->post_excerpt;
+		$link_class = 'magnificPopup-child';
+		$link_icon_class = 'fa-search-plus"';
+		$link = NULL;
+		$image_url = '';
+		$image_caption = NULL;
+		
+		$attachment_query = new WP_Query(array('post_type'=>'attachment', 'posts_per_page'=>1, 'p'=>$attachment));
+		if ( $attachment_query->have_posts() ) {
+			while ( $attachment_query->have_posts() ) { $attachment_query->the_post();
+				$link = $attachment_query->post->guid;
+				$image_url = $attachment_query->post->guid;
+				$image_caption = $attachment_query->post->post_excerpt;
+			}
+		}
+		
+		echo $image_url;
 	?>
 	<a href="<?php echo $link; ?>" title="<?php echo $image_caption; ?>" class="<?php echo $link_class; ?>">
 		<img src="<?php echo acoc_image_size($image_url, TALLYKIT_GALLERY_THUMB_W, TALLYKIT_GALLERY_THUMB_H, true); ?>" width="<?php echo TALLYKIT_GALLERY_THUMB_W; ?>" height="<?php echo TALLYKIT_GALLERY_THUMB_H; ?>" alt="<?php echo $image_caption; ?>"  />
