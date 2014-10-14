5b1e51791fa62ffb003861f62798a4a2f7d6e0b8
Sazzad Hussain <wedcoder09@gmail.com>
2014-10-14 15:47:13 +0600

    Version Bump

--- acoc/acoc.php
+++ acoc/acoc.php
@@ -3,7 +3,7 @@
 Plugin Name: ACOC
 Plugin URI: https://github.com/tallythemes/acoc
 Description: WordPress Plugin Development framework.
-Version: 1.3
+Version: 1.4
 Author: TallyThemes
 Author URI: http://tallythemes.com/
 
