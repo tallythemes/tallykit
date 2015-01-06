bec3e260c2bdfdf91822b3b75d4cc3af307f18a7
TallyThemes <wedcoder09@gmail.com>
2015-01-06 11:28:43 +0600

    Fix a small bug on logo component

--- components/buddypress/buddypress.php
+++ components/buddypress/buddypress.php
@@ -14,9 +14,12 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
+/** Fail silently if WooCommerce is not activated */
+if ( in_array( 'buddypress/bp-loader.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ){	
+	include('buddypress-color.php');
+	include('buddypress-script.php');
+	include('buddypress-template.php');
+	include('buddypress-shortcodes.php');
+	include('buddypress-tinymce.php');
 
-include('buddypress-color.php');
-include('buddypress-script.php');
-include('buddypress-template.php');
-include('buddypress-shortcodes.php');
-include('buddypress-tinymce.php');
+}
